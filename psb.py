c9c19cc9a092bbd924873fca1f6a2d13d8d8e759
mark1code <binomialbats@gmail.com>
2026-01-07 03:24:35 +0000

    Add argument parsing

--- psb.py
+++ psb.py
@@ -1,1 +1,45 @@
-print("Hello World")
+"""
+pre-secure-backup : Proxy Re-Encryption Secure Backup
+"""
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # encrypt command
+    encrypt = subparsers.add_parser("encrypt")
+
+    encrypt.add_argument("--as", dest="user", required=True)
+    encrypt.add_argument("--file", dest="file", required=True)
+    encrypt.add_argument("--vault", dest="vault", required=True)
+
+    # decrypt command
+    decrypt = subparsers.add_parser("decrypt")
+
+    decrypt.add_argument("--as", dest="user", required=True)
+    decrypt.add_argument("--id", dest="object_id", required=True)
+    decrypt.add_argument("--vault", dest="vault", required=True)
+    decrypt.add_argument("--out", dest="out", required=True)
+
+    args = parser.parse_args()
+
+    # test
+    print("command:", args.command)
+
+    if args.command == "encrypt":
+        print("user:  ", args.user)
+        print("file:  ", args.file)
+        print("vault: ", args.vault)
+
+    elif args.command == "decrypt":
+        print("user:  ", args.user)
+        print("id:    ", args.object_id)
+        print("vault: ", args.vault)
+        print("out:   ", args.out)
+
+
+if __name__ == "__main__":
+    main()
